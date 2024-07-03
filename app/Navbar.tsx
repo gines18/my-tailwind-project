ecee2e06330bc47c84089084537c905a7d8692f6
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-03 20:59:54 +0100

    Add navbar and pages

---
+++ app/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Link from 'next/link';
+
+function Navbar() {
+    
+    return (
+<nav className="bg-white shadow dark:bg-gray-800">
+    <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+        <Link href="/dashboard/Home" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</Link>
+
+        <Link href="/dashboard/About" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">About</Link>
+
+        <Link href="/dashboard/Blog" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">blog</Link>
+
+        <Link href="/dashboard/Contact" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact</Link>
+
+    </div>
+</nav>
+  );
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
+import Navbar from './Navbar';
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+  
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+      <Navbar />
+        {children}
+        <Footer />
+        </div>
+        </body>
     </html>
+   
   );
 }
