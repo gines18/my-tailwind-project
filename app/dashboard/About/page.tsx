544ac435bbae3af5f2b57a36219af2b5cf6c998b
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-04 12:26:54 +0100

    "Refactored About and Home pages with updated layout, styling, and content."

--- app/dashboard/About/page.tsx
+++ app/dashboard/About/page.tsx
@@ -1,67 +1,161 @@
-import React from 'react';
-import Image from 'next/image';
-
+import React from "react";
+import Image from "next/image";
 
 export default function About() {
-
   const team = [
     {
       name: "John Doe",
       job: "Software Engineer",
-      face: "/face1.jpg"
+      face: "/face1.jpg",
     },
     {
       name: "Jane Smith",
       job: "Product Manager",
-      face: "/face2.jpg"
+      face: "/face2.jpg",
     },
     {
       name: "Bob Johnson",
       job: "UX Designer",
-      face: "/bob-johnson.jpg"
+      face: "/face3.jpg",
     },
     {
       name: "Alice Williams",
       job: "Data Analyst",
-      face: "/alice-williams.jpg"
+      face: "/face4.jpg",
     },
     {
       name: "Chris Brown",
       job: "Frontend Developer",
-      face: "/chris-brown.jpg"
+      face: "/face5.jpg",
     },
     {
       name: "Eva Green",
       job: "Backend Developer",
-      face: "/eva-green.jpg"
-    }
-  ]
+      face: "/face6.jpg",
+    },
+  ];
 
-  return   (
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Our Team
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              {" "}
+              From concept to launch, we collaborate closely with our clients to
+              bring their vision to life, ensuring that every website we build
+              is a true reflection of their brand and values.
+            </p>
+          </div>
+          <div className="flex flex-wrap">
+            {team.map((item, index) => (
+              <div key={index} className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <Image
+                    alt="team"
+                    src={item.face}
+                    width={100}
+                    height={100}
+                    className="bg-gray-100 object-fit object-center flex-shrink-0 rounded-full mr-4"
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      {item.name}
+                    </h2>
+                    <p className="text-gray-500">{item.job}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
+            Testimonials
+          </h1>
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="h-full bg-gray-100 p-8 rounded">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="block w-5 h-5 text-gray-400 mb-4"
+                  viewBox="0 0 975.036 975.036"
+                >
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+                <p className="leading-relaxed mb-6">
+                  "Working with the team to develop our company website has been
+                  a game-changer. Their expertise in website design and
+                  optimization has transformed our online presence. We've seen a
+                  significant increase in traffic and engagement, and our site
+                  truly reflects our brand identity now. Highly recommended!
+                </p>
+                <a className="inline-flex items-center">
+                  <Image
+                    alt="testimonial"
+                    src="/testimonial1.jpg"
+                    width={70}
+                    height={70}
+                    className="rounded-full flex-shrink-0 object-cover object-center"
+                  />
+                  <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-gray-900">
+                      John Smith
+                    </span>
+                    <span className="text-gray-500 text-sm">
+                      {" "}
+                      CEO of Tech Innovators Inc.
+                    </span>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="h-full bg-gray-100 p-8 rounded">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="block w-5 h-5 text-gray-400 mb-4"
+                  viewBox="0 0 975.036 975.036"
+                >
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+                <p className="leading-relaxed mb-6">
+                  The process of crafting our website content was seamless and
+                  highly effective. The team understood our vision and delivered
+                  compelling content that perfectly communicates our value
+                  proposition. Our website has become a key tool in driving
+                  conversions and engaging with our audience. We couldn't be
+                  happier with the results!
+                </p>
+                <a className="inline-flex items-center">
+                <Image
+                    alt="testimonial"
+                    src="/testimonial2.jpg"
+                    width={70}
+                    height={70}
+                    className="rounded-full flex-shrink-0 object-cover object-center"
+                  />
+                  <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-gray-900">
+                      Alper Kamu
+                    </span>
+                    <span className="text-gray-500 text-sm">DESIGNER</span>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       
-    <section className="text-gray-600 body-font">
-    <div className="container px-5 py-24 mx-auto">
-      <div className="flex flex-col text-center w-full mb-20">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base"> From concept to launch, we collaborate closely with our clients to bring their vision to life, ensuring that every website we build is a true reflection of their brand and values.</p>
-      </div>
-<div className="flex flex-wrap">
-{team.map((item, index) => (
-  <div key={index} className="p-2 lg:w-1/3 md:w-1/2 w-full">
-    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-      <Image alt="team" src={item.face} width={80} height={80} className="bg-gray-100 object-fit object-center flex-shrink-0 rounded-full mr-4"/>
-      <div className="flex-grow">
-        <h2 className="text-gray-900 title-font font-medium">{item.name}</h2>
-        <p className="text-gray-500">{item.job}</p>
-      </div>
-    </div>
-  </div>
-))}
-</div>
-    </div>
-  </section>
-  )
-  
-  
+    </>
+  );
 }
-
--- app/dashboard/Home/page.tsx
+++ app/dashboard/Home/page.tsx
@@ -28,39 +28,9 @@ export default function Home() {
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-               Read more
+                Read more
               </button>
-         
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">
-            Creating a Strong Online Presence: Key to Success in the Digital Age
-            </h1>
-            <p className="mb-8 leading-relaxed">
-            In today's digital era, establishing a robust online presence is essential for individuals and businesses alike. An online presence encompasses various digital footprints, including websites, social media profiles, and content contributions across different platforms.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                First step
-              </button>
-            
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <Image
-              src="/marketing.jpg"
-              alt="hero"
-              layout="fixed"
-              width={500}
-              height={300}
-              className="object-cover object-center rounded"
-            />
           </div>
         </div>
       </section>
@@ -70,22 +40,26 @@ export default function Home() {
           <div className="flex flex-wrap -m-4">
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
+                <Image
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
                   src="/design.jpg"
                   alt="blog"
+                  layout="fixed"
+                  width={500}
+                  height={300}
                 />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  Website Design
+                    Website Design
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Crafting the Perfect Website Design
+                    Crafting the Perfect Website Design
                   </h1>
                   <p className="leading-relaxed mb-3">
-                  Learn the principles of creating a visually appealing and user-friendly website. Discover design techniques that reflect your brand identity, engage visitors, and provide a seamless user experience.
-
-
+                    Learn the principles of creating a visually appealing and
+                    user-friendly website. Discover design techniques that
+                    reflect your brand identity, engage visitors, and provide a
+                    seamless user experience.
                   </p>
                   <div className="flex items-center flex-wrap ">
                     <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
@@ -103,7 +77,6 @@ export default function Home() {
                         <path d="M12 5l7 7-7 7"></path>
                       </svg>
                     </a>
-                 
                   </div>
                 </div>
               </div>
@@ -111,20 +84,26 @@ export default function Home() {
 
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
+                <Image
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
                   src="/content.jpg"
                   alt="blog"
+                  layout="fixed"
+                  width={500}
+                  height={300}
                 />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  Website Content
+                    Website Content
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Developing Compelling Website Content
+                    Developing Compelling Website Content
                   </h1>
                   <p className="leading-relaxed mb-3">
-                  Unlock the power of content to communicate your company's value proposition. Learn how to create engaging, informative, and persuasive content that resonates with your target audience and drives conversions.
+                    Unlock the power of content to communicate your company's
+                    value proposition. Learn how to create engaging,
+                    informative, and persuasive content that resonates with your
+                    target audience and drives conversions.
                   </p>
                   <div className="flex items-center flex-wrap">
                     <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
@@ -142,7 +121,6 @@ export default function Home() {
                         <path d="M12 5l7 7-7 7"></path>
                       </svg>
                     </a>
-               
                   </div>
                 </div>
               </div>
@@ -150,20 +128,26 @@ export default function Home() {
 
             <div className="p-4 md:w-1/3">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img
+                <Image
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
                   src="/performance.jpg"
                   alt="blog"
+                  layout="fixed"
+                  width={500}
+                  height={300}
                 />
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  Website Optimization
+                    Website Optimization
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Enhancing Website Performance
+                    Enhancing Website Performance
                   </h1>
                   <p className="leading-relaxed mb-3">
-                  Optimize your website for speed and search engine rankings. Explore best practices for improving load times and SEO to ensure your site performs at its best and reaches a wider audience.
+                    Optimize your website for speed and search engine rankings.
+                    Explore best practices for improving load times and SEO to
+                    ensure your site performs at its best and reaches a wider
+                    audience.
                   </p>
                   <div className="flex items-center flex-wrap ">
                     <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
@@ -181,7 +165,6 @@ export default function Home() {
                         <path d="M12 5l7 7-7 7"></path>
                       </svg>
                     </a>
-               
                   </div>
                 </div>
               </div>
@@ -189,6 +172,180 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              1
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Define Your Goals
+                </h2>
+                <p className="leading-relaxed">
+                  Understand what you want to achieve with your website, whether
+                  it's increasing brand visibility, driving sales, or providing
+                  information.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              2
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Choose the Right Platform
+                </h2>
+                <p className="leading-relaxed">
+                  Select a website building platform that suits your needs and
+                  technical expertise, ensuring ease of use and scalability.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              3
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx="12" cy="5" r="3"></circle>
+                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Design with Your Audience in Mind
+                </h2>
+                <p className="leading-relaxed">
+                  Create a user-friendly and visually appealing design that
+                  resonates with your target audience and enhances their
+                  experience.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
+              4
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Optimize for Performance and SEO
+                </h2>
+                <p className="leading-relaxed">
+                  Ensure your website loads quickly, is mobile-friendly, and
+                  optimized for search engines to reach a broader audience and
+                  improve visibility.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">
+              Creating a Strong Online Presence: Key to Success in the Digital
+              Age
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              In today's digital era, establishing a robust online presence is
+              essential for individuals and businesses alike. An online presence
+              encompasses various digital footprints, including websites, social
+              media profiles, and content contributions across different
+              platforms.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                First step
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <Image
+              src="/marketing.jpg"
+              alt="hero"
+              layout="fixed"
+              width={500}
+              height={300}
+              className="object-cover object-center rounded"
+            />
+          </div>
+        </div>
+      </section>
+
     </>
   );
 }
