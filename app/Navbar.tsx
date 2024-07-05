8cb58a1f936d49ac4d212ccaf832ab04ec688905
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-05 17:06:45 +0100

    "Updated Navbar component: changed logo text to 'Website name', renamed 'About' to 'Home' and 'Blog' to 'About' in navigation links."

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -10,11 +10,11 @@ function Navbar() {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span className="ml-3 text-xl">Tailblocks</span>
+      <span className="ml-3 text-xl">Website name</span>
     </a>
     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <Link   href="/dashboard/Home" className="mr-5 hover:text-gray-900">About</Link>
-      <Link   href="/dashboard/About" className="mr-5 hover:text-gray-900">Blog</Link>
+      <Link   href="/dashboard/Home" className="mr-5 hover:text-gray-900">Home</Link>
+      <Link   href="/dashboard/About" className="mr-5 hover:text-gray-900">About</Link>
       <Link   href="/dashboard/Projects" className="mr-5 hover:text-gray-900">Projects</Link>
       <Link   href="/dashboard/Contact" className="mr-5 hover:text-gray-900">Contact</Link>
     </nav>
