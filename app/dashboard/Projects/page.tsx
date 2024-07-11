0bd7ef205f7bcf97dff1889d62e87fd623d09278
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-11 16:11:41 +0100

    "Updated button to be a link to /dashboard/Contact and changed button text to 'Contact me'"

--- app/dashboard/Projects/page.tsx
+++ app/dashboard/Projects/page.tsx
@@ -308,7 +308,9 @@ export default function Main() {
         </div>
       </div>
     </div>
-    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+    <Link href="/dashboard/Contact"> 
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Contact me</button>
+    </Link>
   </div>
 </section>
 
