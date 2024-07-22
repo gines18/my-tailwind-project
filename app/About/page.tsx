07859e890d5c904ba2041e9df860f229a65d80c7
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-07-22 13:11:40 +0100

    Update text color of 'Testimonials' heading from gray-900 to gray-800

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -136,7 +136,7 @@ export default function About() {
 
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
+          <h1 className="text-3xl font-medium title-font text-gray-800 mb-12 text-center">
             Testimonials
           </h1>
           <div className="flex flex-wrap -m-4">
