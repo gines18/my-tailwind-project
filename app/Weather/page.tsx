67ff646dba486b6948bf6ed0ea138df05d8fed36
Mariusz Ruczkowski <mario@Mariuszs-Mini.lan>
2024-09-20 22:13:36 +0100

    Added image import and handling for weather icons, updated weather API fetch to include max temperature and icon, and configured Next.js to allow images from openweathermap.org

--- app/Weather/page.tsx
+++ app/Weather/page.tsx
@@ -1,25 +1,41 @@
 'use client'
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image'; // Add this import at the top
 
 async function fetchWeather(city: string) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=732a88c6f9f8186453f47b435c9ba5f3`;
   const response = await fetch(url);
   const data = await response.json();
   const temperature = data.list[0].main.temp - 273.15;
-  return temperature ;
+  const temperatureMax = data.list[0].main.temp_max - 273.15; // Corrected from pressure to temp_max
+  const icon = data.list[0].weather[0].icon; // Corrected to access the first weather object
+  return [ temperature, temperatureMax, icon ];
 }
 
 function WeatherPage() {
   const [city, setCity] = useState('');
   const [temperature, setTemperature] = useState<number | null>(null);
-
+  const [temperatureMax, setTemperatureMax] = useState<number | null>(null);
+  const [icon, setIcon] = useState<string | null>(null)
+  
   useEffect(() => {
-    const fetchTemperature = async () => {
-      const temp: number = await fetchWeather(city);
-      setTemperature(temp);
+    const fetchWeatherData = async () => {
+      try {
+        const [temp, tempMax, fetchedIcon ] = await fetchWeather(city);
+        setTemperature(temp);
+        setTemperatureMax(tempMax);
+        setIcon(fetchedIcon);
+      } catch (error) {
+        console.error(error);
+        setTemperature(null);
+        setTemperatureMax(null);
+        setIcon(null);
+      }
     };
 
-    fetchTemperature();
+    if (city) {
+      fetchWeatherData();
+    }
   }, [city]);
 
   return (
@@ -31,11 +47,13 @@ function WeatherPage() {
         onChange={(e) => setCity(e.target.value)}
         placeholder="Enter city name"
         className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
-      />
-          {temperature ? <p className='text-lg font-semibold'>Temperature: {temperature.toFixed(2)} °C</p> : <p className='text-lg font-semibold'>Loading...</p>}
+      />  
+          {icon ? <Image src={`http://openweathermap.org/img/wn/${icon}.png`} width={70} height={70} alt="Weather icon" /> : <p></p>}
+          {temperature ? <p className='text-lg font-semibold'>Temperature: {temperature.toFixed(2)} °C</p> : <p className='text-lg font-semibold'>Temperature ...</p>}
+          {temperatureMax ? <p className='text-lg font-semibold'>Max temperature: {temperatureMax.toFixed(2)} °C</p> : <p className='text-lg font-semibold'>Max temperature ...</p>}
     </div>
   );
 }
 
 export default WeatherPage;
-  
+
